bae534e26ddc789da96d34b4a0d8e824378147cd
Siddhu107 <164214378+Siddhu107@users.noreply.github.com>
2026-04-24 17:39:41 +0530

    added env

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,12 +2,24 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    const conn = await mongoose.connect(
+      process.env.MONGO_URI
+    );
+
+    console.log(
+      `MongoDB Connected: ${conn.connection.host}`
+    );
+
   } catch (error) {
-    console.error("DB connection error:", error.message);
+
+    console.error(
+      "DB connection error:",
+      error.message
+    );
+
     process.exit(1);
+
   }
 };
 
