a7b635215bb24ba315563786910f8d0c8506fe41
Siddhu107 <164214378+Siddhu107@users.noreply.github.com>
2026-04-29 09:58:53 +0530

    fixed vendor routes

--- backend/routes/vendorRoutes.js
+++ backend/routes/vendorRoutes.js
@@ -1,20 +1,46 @@
 import express from "express";
-import { registerVendor, loginVendor,addMenu,getAllVendors,getVendorDetails , updateVendorProfile, updateVendorPassword} from "../controllers/vendorController.js";
+
+import {
+  registerVendor,
+  loginVendor,
+  getAllVendors,
+  getVendorDetails,
+  updateVendorProfile,
+  updateVendorPassword
+} from "../controllers/vendorController.js";
+
 import { protectVendor } from "../middlewares/authMiddleware.js";
+
 import upload from "../middlewares/uploadMiddleware.js";
 
 const vendorroutes = express.Router();
 
-vendorroutes.post("/register", registerVendor);
-vendorroutes.post("/login", loginVendor);
-vendorroutes.get("/all", getAllVendors);
-vendorroutes.post("/register",upload.single("profilePic"),registerVendor);
+// =========================
+// PUBLIC ROUTES
+// =========================
+
+// Register Vendor (with profile pic)
+vendorroutes.post( "/register",upload.single("profilePic"),registerVendor);
+
+// Login Vendor
+vendorroutes.post("/login",loginVendor);
+
+// Get all vendors (user browsing)
+vendorroutes.get("/all",getAllVendors);
+
+// =========================
+// PROTECTED ROUTES
+// =========================
 
 vendorroutes.use(protectVendor);
 
-vendorroutes.get("/:id/details", getVendorDetails);
-vendorroutes.put("/update-profile", updateVendorProfile);
-vendorroutes.put("/change-password", updateVendorPassword);
+// Get Vendor Details
+vendorroutes.get("/:id/details",getVendorDetails);
 
+// Update Profile
+vendorroutes.put("/update-profile",updateVendorProfile);
+
+// Change Password
+vendorroutes.put("/change-password",updateVendorPassword);
 
 export default vendorroutes;
