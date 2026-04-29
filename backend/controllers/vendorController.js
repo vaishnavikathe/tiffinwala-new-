2b58db2c102a9fad49dd879e5e8baf300574b385
Siddhu107 <164214378+Siddhu107@users.noreply.github.com>
2026-04-29 10:08:17 +0530

    minor changes

--- backend/controllers/vendorController.js
+++ backend/controllers/vendorController.js
@@ -132,7 +132,7 @@ export const getAllVendors = async (req, res) => {
   try {
     // ✅ query params
     const page = parseInt(req.query.page) || 1;
-    const limit = parseInt(req.query.limit) || 10;
+    const limit = parseInt(req.query.limit) || 9;
 
     const skip = (page - 1) * limit;
 
