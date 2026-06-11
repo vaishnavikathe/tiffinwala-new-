32e45a16cf8861ffeb1cdc6ab91c7dd46df896d2
Siddhesh <sidjadhav1072005@gmail.com>
2026-06-11 14:20:15 +0530

    updated pause subscription

--- backend/controllers/subscriptionController.js
+++ backend/controllers/subscriptionController.js
@@ -494,25 +494,23 @@ export const cancelSubscription = async (req, res) => {
 
 export const pauseTiffin = async (req, res) => {
   try {
-    const { date } = req.body; // Date jo pause karni hai
-    const sub = await Subscription.findById(req.params.id);
-    
-    if (!sub) return res.status(404).json({ message: "Not found" });
-  
-    const alreadyPaused = sub.pausedDates.some(
-      d => new Date(d).toDateString() === new Date(date).toDateString()
-    );
-    
-    if (alreadyPaused) {
-      return res.status(400).json({ message: "Already paused for this date" });
-    }
-    
-    sub.pausedDates.push(new Date(date));
-    sub.pausedDays += 1;
+    const { fromDate, toDate } = req.body;
 
-    sub.endDate = new Date(sub.endDate.getTime() + 24 * 60 * 60 * 1000);
-    
-    await sub.save();
+const from = new Date(fromDate);
+const to = new Date(toDate);
+const days = Math.ceil((to - from) / (1000 * 60 * 60 * 24)) + 1;
+
+
+let current = new Date(from);
+while (current <= to) {
+  sub.pausedDates.push(new Date(current));
+  current.setDate(current.getDate() + 1);
+}
+
+sub.pausedDays += days;
+// End date 
+sub.endDate = new Date(sub.endDate.getTime() + days * 24 * 60 * 60 * 1000);
+await sub.save();
     res.json({ message: "Tiffin paused!", subscription: sub });
     
   } catch (err) {
